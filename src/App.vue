5773abff8d753234d6943afb4ffd686f16157ae3
Jacob Albright <jacob.albright23@gmail.com>
2018-10-27 18:05:36 -0500

    some really rough bootstrap styling and canvas drawing image

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,42 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="container-fluid">
+    <div class="row">
+      <div id="nav">
+        
+        <router-link :to="{name:'home'}">Home</router-link> | 
+        <router-link :to="{name:'about'}">About</router-link>
+      </div>
     </div>
-    <router-view/>
+    <div class="row">
+      <div id="controls">
+        <router-view></router-view>
+      </div>
+      <div class="col-xs-8">
+        <game-board></game-board>
+      </div>
+    </div>
+   
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import gameBoard from '@/components/Board'
+export default {
+  components: {
+    gameBoard,
+  }
 }
+</script>
+
+
+<style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+
+#controls {
+  width: 400px;
+  background-color: beige;
+}
+
 #nav {
   padding: 30px;
   a {
---
+++ src/components/Board.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <canvas style="margin: 0px 0px;" ref="canvas"></canvas>
+  </div>
+</template>
+
+<script>
+import boardImgSRC from '../assets/pandemic-board.png'
+export default {
+  mounted () {
+    const canvas = this.$refs.canvas;    
+    canvas.width = 900;
+    canvas.height = 600;
+    const ctx = canvas.getContext('2d');
+    const boardImg2 = new Image()
+    boardImg2.src = boardImgSRC;    
+    boardImg2.onload = function(e) {
+      ctx.drawImage(this, 0, 0, 900, 600)
+    }
+  },
+}
+</script>
